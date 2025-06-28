3b51b7efa41bfc32922598ae64346cc2afc9823c
damnis <36800988+damnis@users.noreply.github.com>
2025-06-28 09:36:42 +0200

    Update SAM-app.py

--- SAM-app.py
+++ SAM-app.py
@@ -166,17 +166,21 @@ def calculate_sam(df):
     
     # --- SAMD op basis van DI+ en DI- ---
     # --- SAMD op basis van DI+ en DI- ---
-    adx = ADXIndicator(high=df["High"], low=df["Low"], close=df["Close"], window=14)
+    high_series = df["High"].squeeze()
+    low_series = df["Low"].squeeze()
+    close_series = df["Close"].squeeze()
+
+    adx = ADXIndicator(high=high_series, low=low_series, close=close_series, window=14)
 
     df["DI_PLUS"] = adx.adx_pos()
     df["DI_MINUS"] = adx.adx_neg()
-
-    df["SAMD"] = 0.0  # standaardwaarde
+    df["SAMD"] = 0.0
 
     df.loc[(df["DI_PLUS"] > 0) & (df["DI_MINUS"] == 0), "SAMD"] = 1.0
     df.loc[(df["DI_MINUS"] > 0) & (df["DI_PLUS"] == 0), "SAMD"] = -1.0
     df.loc[(df["DI_PLUS"] > df["DI_MINUS"]) & (df["DI_MINUS"] > 0), "SAMD"] = 0.5
     df.loc[(df["DI_MINUS"] > df["DI_PLUS"]) & (df["DI_PLUS"] > 0), "SAMD"] = -0.5
+
     
     # samd oud
 #    df["daily_range"] = df["High"] - df["Low"]
